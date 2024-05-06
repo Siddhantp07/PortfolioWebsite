d0be73d23286686db3d61a9d1cef941337df853e
Siddhantp07 <168208033+Siddhantp07@users.noreply.github.com>
2024-05-06 13:11:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+<script>
+    letnav=document.getElementById('nav').style;
+letopenMenu=function(){
+if(nav.visibility="hidden"){
+nav.visibility="visible";
+    }
+
+}
+letcloseMenu=function(){
+nav.visibility="hidden";
+ }
+let submit=function(){
+alert('thanks for the submission');
+}
+
+</script>
